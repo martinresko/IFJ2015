f7bae55cc6d51b026d0874d192034992642b47f4
Dominik <xhlava42@stud.fit.vutbr.cz>
2015-11-07 22:33:51 +0100

    Added concatenation and string length

--- ial.c
+++ ial.c
@@ -16,63 +16,103 @@
 #include <stdlib.h>
 #include <string.h>
 
-int find (char *string, char *pattern);
+/**Prototypy funkcii**/
+int find(char *string, char *pattern);
 int *KMPGraf(char *pattern, int pattern_length);
+int length_of_string(char * string);
+char* concatenation(char *string1, char *string2);
 
-int *KMPGraf(char *pattern, int pattern_length)
+
+/**Vektor chyby**/
+int *KMPGraf(char *pattern, int pattern_length) 
 {
 	int k,r;
-	int *pi = malloc(sizeof(int)*pattern_length);
+	int *pi = malloc(sizeof(int) * pattern_length);
 
-	if(pi == NULL) {
-		return NULL;	 
+	if (pi == NULL) {
+		return NULL;
 	}
 
-	pi[1] = 0;
+	pi[0] = 0;
 
-	for (k = 2; k < pattern_length; k++) {
-		r = pi[k-1];
-		while ((r > 0) && (pattern[r] != pattern[k-1])) {
+	for (k = 1; k < pattern_length; k++) {
+		r = pi[k - 1];
+
+		while ((r > 0) && (pattern[r ] != pattern[k - 1])) {
 			r = pi[r];
 		}
-		pi[k] = r+1;;
+
+		pi[k] = r + 1;
 	}
+	
 	return pi;
 }
 
+/**Implementacia Knutth-Morris-Pratt algoritmu**/
 int KMP(char *string, char *pattern)
 {
 
 	int tindex = 1;
 	int pindex = 1;
-	int string_length = strlen(string);
-    int pattern_length = strlen(pattern);
+	int string_length = strlen(string); // dlzka retazca
+    int pattern_length = strlen(pattern); // dlzka podretazca
     int *pi = KMPGraf(string, pattern_length);
 
-    while ((tindex<=string_length) && (pindex <=pattern_length)) {
-    	if ((pindex == 0) || (string[tindex] == pattern[pindex])) {
+    while ((tindex <= string_length) && (pindex <= pattern_length)) {
+    	if ((pindex == 0) || (string[tindex - 1] == pattern[pindex - 1])) { // vracia prvy vyskyt
     		pindex++;
     		tindex++;
     	}
     	else {
-    		pindex = pi[pindex];
+    		pindex = pi[pindex - 1];
     	}
     }
 
-    if (pindex>pattern_length) {
-    	return (tindex-pattern_length);
+    free(pi);
+    pi = NULL;
+	
+    if (pindex > pattern_length) {
+    	return ((tindex - pattern_length) - 1);
     }
     else {
     	return -1;
     }
 }
 
+/**Implementacia algoritmu na zistovanie dlzky retazca**/
+int length_of_string(char * string)
+{
+	int string_length = strlen(string); // dlzka retazca	
+	return string_length;
+}
+
+/**Implementacia konkatenancie dvoch retazcov **/
+char* concatenation(char *string1, char *string2)
+{
+	char *result = malloc(strlen(string1) + strlen(string2) + 1);
+
+	if (result == NULL) {
+		return NULL;
+	}
+
+	strcpy(result, string1);
+    strcat(result, string2);
+
+    return result;
+} 
 
 
 
 int main() 
 {
-	printf("%d\n", KMP("kokotar", "ar"));
-
+	char * str = concatenation("ahoj", "jano");
+	printf("%s\n",str );
+	free (str);
+	printf("%d\n", length_of_string("x\nz"));
+	printf("%d\n", KMP("cabcabcab", "ab"));
+	printf("%d\n", KMP("cccabcdefab", "de"));
+	printf("%d\n", KMP("cccabcdefab", "c"));
+	printf("%d\n", KMP("cccabcdefab", "aasdasdasda"));
+	printf("%d\n", KMP("cccabcdefab", "fab"));
 	return 0;
 }
