1d0cf06cd1d3f433ad86de432cd96eb593830bba
domoo <david.domonkos95@gmail.com>
2015-12-07 19:31:28 +0100

    interpret zakladna kostra

--- interpret.c
+++ interpret.c
@@ -20,3 +20,111 @@ ERROR_CODE init_interpret(Table_Symbols *table){
 	run_error = interpret()
 }
 
+ERROR_CODE interpret(){
+
+	while(run_error == OK_ERR){
+		switch(){
+			case iMOV:
+			break;
+
+			case iADD:
+			break;
+
+			case iSUB:
+			break;
+
+			case iMUL:
+			break;
+
+			case iDIV:
+			break;
+
+			case iGREATER:
+			break;
+
+			case iLESS:
+			break;
+
+			case iEGREATER;
+			break;
+
+			case iELESS:
+			break;
+
+			case iEQUAL:
+			break;
+
+			case iNEQUAL:
+			break;
+
+			case iJMP:
+			break;
+
+			case iJZ:
+			break;
+
+			case iJNZ:
+			break;
+
+			case iCALL:
+			break;
+
+			case iSORT:
+			break;
+
+			case iLEN:
+			break;
+
+			case iSUBSTR:
+			break;
+
+			case iFIND:
+			break;
+
+			case iCONCAT:
+			break;
+
+			case iWRITE:
+			break;
+
+			case iREAD:
+			break;
+
+			case iPUSH:
+			break;
+
+			case iPOP:
+			break;
+
+			case iTOP:
+			break;
+
+			case iTOPPOP:
+			break;
+
+			case iEMPTY:
+			break;
+
+			case iINIT:
+			break;
+
+			case iRET:
+			break;
+
+			case iLABEL:
+			break;
+
+			case iINITFR:
+			break;
+
+			case iDISPFR:
+			break;
+
+			case iWRITEFR:
+			break;
+
+			case iREADFR:
+			break;
+		}
+	}
+}
